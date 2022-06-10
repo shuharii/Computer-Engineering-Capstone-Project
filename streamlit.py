fdd9cdb71fb4ada83f69efcaae2e2cf559db8707
eren gunestas <44318077+shuharii@users.noreply.github.com>
2022-06-10 10:43:24 +0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -11,25 +11,25 @@ st.title("W E L C O M E !")
 
 st.title("Please select the concrete features you want to produce.!")
 
-cement = st.sidebar.slider('Cement:', 0, 1300, 0)
+cement = st.sidebar.number_input('Cement:')
 st.write(cement, ' kg/m^3 cement selected.')
 
-flyash = st.sidebar.slider('Fly Ash', 0, 1300, 0)
+flyash = st.sidebar.number_input('Fly Ash')
 st.write(flyash, ' kg/m^3 fly ash selected.')
 
-water = st.sidebar.slider('Water', 0, 1300, 0)
+water = st.sidebar.number_input('Water')
 st.write(water, ' kg/m^3 water selected.')
 
-superplasticizer = st.sidebar.slider('Superplasticizer', 0, 1300, 0)
+superplasticizer = st.sidebar.number_input('Superplasticizer')
 st.write(superplasticizer, ' kg/m^3 superplasticizer selected.')
 
-coarse_aggregate = st.sidebar.slider('Coarse Aggregate', 0, 1300, 0)
+coarse_aggregate = st.sidebar.number_input('Coarse Aggregate')
 st.write(coarse_aggregate, ' kg/m^3 coarse_aggregate selected.')
 
-fine_aggregate = st.sidebar.slider('Fine Aggregate', 0, 1300, 0)
+fine_aggregate = st.sidebar.number_input('Fine Aggregate')
 st.write(fine_aggregate, ' kg/m^3 fine_aggregate selected.')
 
-age = st.sidebar.slider('Age',1, 1300, 1)
+age = st.sidebar.number_input('Age')
 st.write(fine_aggregate, ' days selected.')
 
 a={'s':cement,'a':flyash,'k':water,'j':superplasticizer,'l':coarse_aggregate,'m':fine_aggregate,'n':age}
@@ -41,4 +41,4 @@ manuel_model = keras.models.load_model('my_model.h5')
 if st.sidebar.button('Show Strength of Concrete'):
     ypred = manuel_model.predict(X_manuel_test)
     st.title('Strength of concrete produced : ')
-    st.title(np.round(ypred[0]))
+    st.title(ypred[0])
