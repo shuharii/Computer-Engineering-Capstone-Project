a3ef9b3d4a217eded6268cf0f9b98bcbe10a3572
eren gunestas <44318077+shuharii@users.noreply.github.com>
2022-06-10 09:13:33 +0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import numpy as np
 import streamlit as st
+import pandas as pd
 #from tensorflow.keras.callbacks import EarlyStopping
 #from sklearn.model_selection import train_test_split
 from tensorflow import keras
